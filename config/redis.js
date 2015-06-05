9b9bb3769961b59f1fdafb6cda2f5e7196b4e933
MirrorJing <zfeidy@qq.com>
2015-06-05 10:23:38 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	AndroidManifest.xml
    	bin/AndroidManifest.xml.d
    	local.properties
    	res/drawable/fillet_shape.xml
    	res/layout/dongxin_title.xml
    	res/layout/user_list.xml
    	res/layout/user_login.xml
    	res/values/strings.xml
    	res/values/styles.xml
    	src/com/jd/dongxin/base/BaseApplication.java
    	src/com/jd/dongxin/base/WelcomeGuide.java
    	src/com/jd/dongxin/user/UserLogin.java
    	src/com/jd/dongxin/util/cache/JdDongXinCache.java
    	src/com/jd/dongxin/util/cache/JdDongXinSP.java

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,7 @@
 var config = {
     // redis 配置，默认是本地
-//    redis_host: '192.168.155.103',
-    redis_host: 'localhost',
+    redis_host: '192.168.155.103',
+//    redis_host: 'localhost',
     redis_port: 6379,
     redis_db: 0,
     session_secret: 'jd_come_on_baby@618',
