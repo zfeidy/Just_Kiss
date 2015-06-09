2f7ecc83fef9e4206e6d7daa2303154518d03e57
MirrorJing <zfeidy@qq.com>
2015-06-09 21:56:28 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- router.js
+++ router.js
@@ -2,9 +2,11 @@ var config = require('./config/setting');
 var user = require('./controller/user');
 var kissme = require('./controller/kissme');
 var employee = require('./controller/employee');
+var auth = require('./middleware/auth');
+var visit = require('./middleware/visits');
 
 module.exports = function (app) {
-    
+
     // 基本操作
     app.get('/', user.index);
     app.get('/login', user.login);
@@ -13,14 +15,19 @@ module.exports = function (app) {
     app.get('/home', user.home);
     app.get('/chat', user.chat);
 
-    // kissme
+    // kissme [ejs渲染]
     app.get('/kissme', kissme.index);
     app.get('/kissme/kiss', kissme.kisslocal);
     app.post('/kissme/kiss', kissme.kiss);
     app.get('/kissme/random', kissme.random);
-    
-    // employee
-    app.get('/employee/add', employee.add);
-    app.post('/employee/add', employee.doAdd);
-    app.get('/employee/list', employee.list);
+
+    // employee [ajax渲染]
+    app.get('/employee/add', auth.authUser, employee.add);
+    app.post('/employee/add', auth.authUser, employee.doAdd);
+    app.get('/employee/list', auth.authUser, employee.list);
+    app.get('/employee/kiss', employee.kisslocal);
+    app.post('/employee/kiss', employee.kiss);
+    app.get('/employee/random', visit.count, employee.random);
+    app.get('/employee/count', employee.count);
+//    app.get('/employee/random', employee.random);
 };
