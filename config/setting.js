00ac37ffe33b310ee29d7f6b6ce3ed503ebea516
zfeidy <zfeidy@qq.com>
2015-06-12 01:50:10 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	views/index.html

--- config/setting.js
+++ config/setting.js
@@ -2,14 +2,15 @@ var path = require('path');
 
 var config = {
     // debug 为 true 时，用于本地调试
-    debug: true,
+    debug: false,
     name: '加油呗比!',
     description: '京东618，加油你我他！',
-    groupsize: 1000,
+    groupsize: 10000,
     keywords: '618大促,京东',
     number: 4, //一次出现图片的张数
     expires: 360, // cookie过期时间。默认配置【0永久，1（小时），6（小时），12（小时），24（小时），48（小时），72（小时），168（小时）,360(小时),720(小时)】
     use_mongo: false, // 是否使用mongodb做存储
+    log_path: '/log',
     // 程序运行的端口
     port: 3000,
     admin: "21232f297a57a5a743894a0e4a801fc3",
