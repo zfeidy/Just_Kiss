ba1ff5336741b70311851eadeaf873e5fbe25165
MirrorJing <zfeidy@qq.com>
2015-06-13 04:58:35 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- config/redis.js
+++ config/redis.js
@@ -1,19 +1,2 @@
-var config = {
-    "redis_host": "192.168.192.38",
-    "redis_port": 5360,
-    "opts": {
-        "hostname": "aps.jimdb.jd.local",
-        "host": "aps.jimdb.jd.local",
-        "path": "/get",
-        "method": "get",
-        "port": 80
-    },
-    "use_cluster": true,
-    "redis_db": 0,
-    "redis_auth": "/redis/cluster/19:1417632068813",
-    "cluster": [
-        {"port": "5360", "host": "192.168.192.38"},
-        {"port": "5360", "host": "192.168.192.39"}
-    ]
-};
+var config = {"redis_host":"192.168.192.39","redis_port":"5360","opts":{"hostname":"aps.jimdb.jd.local","host":"aps.jimdb.jd.local","path":"/get","method":"get","port":80},"use_cluster":true,"redis_db":0,"redis_auth":"/redis/cluster/19:1417632068813","cluster":[{"port":"5360","host":"192.168.192.39"},{"port":"5360","host":"192.168.192.38"}]};
 module.exports = config;
