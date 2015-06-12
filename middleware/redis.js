82f60b80a0f780aba3d4665fbd2e74c2d22c74e3
MirrorJing <zfeidy@qq.com>
2015-06-13 00:28:28 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

---
+++ middleware/redis.js
@@ -0,0 +1,62 @@
+var config = require("../config/test");
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+exports.initRedisCluster = function () {
+    console.log("init redis cluster start......");
+// 初始化redis集群
+    initRedis();
+    console.log("init redis cluster end！");
+};
+
+var sleep = function (sleepTime) {
+    for (var start = +new Date; +new Date - start <= sleepTime; ) {
+    }
+};
+
+var initRedis = function () {
+    if (config.use_cluster && !config.cluster) {
+        // 初始化redis
+        var options = {
+            hostname: config.opts.hostname,
+            port: config.opts.port,
+            method: config.opts.method,
+            path: config.opts.path
+        };
+        console.log(options);
+        var getRedisReq = http.request(options, function (res) {
+            console.log("get redis");
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log(chunk);
+                var clusterStr = chunk.split("\n");
+                var opts = [];
+                for (var i in clusterStr) {
+                    var tmp = clusterStr[i].split(":");
+                    if (tmp.length > 1) {
+                        opts.push({
+                            port: tmp[1],
+                            host: tmp[0]
+                        });
+                    }
+                }
+                config.cluster = opts;
+                var newConfig = "var config = " + JSON.stringify(config);
+                newConfig += ";\n";
+                newConfig += "module.exports = config;";
+                fs.writeFile('../config/test.js', newConfig, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            });
+        }).on('error', function (e) {
+            console.log("获取失败: " + e.message);
+            console.log(e);
+        });
+        getRedisReq.end();
+    } else {
+        console.log("loading...");
+    }
+};
