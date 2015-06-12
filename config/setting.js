d9f94265529c6b014b24da8cbba43180011bfd41
MirrorJing <zfeidy@qq.com>
2015-06-13 04:47:01 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- config/setting.js
+++ config/setting.js
@@ -2,11 +2,8 @@ var path = require('path');
 
 var config = {
     // debug 为 true 时，用于本地调试
-    debug: true,
-    name: '加油呗比!',
-    description: '京东618，加油你我他！',
+    debug: false,
     groupsize: 1000,
-    keywords: '618大促,京东',
     number: 4, //一次出现图片的张数
     expires: 360, // cookie过期时间。默认配置【0永久，1（小时），6（小时），12（小时），24（小时），48（小时），72（小时），168（小时）,360(小时),720(小时)】
     use_mongo: false, // 是否使用mongodb做存储
