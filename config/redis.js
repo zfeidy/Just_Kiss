8fe74cfb7c0501d94cb16d165cb0dbd7e5ab0be5
MirrorJing <zfeidy@qq.com>
2015-06-13 00:28:09 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,21 @@
 var config = {
     // redis 配置，默认是本地
-    redis_host: '192.168.155.103',
-//    redis_host: 'localhost',
-    redis_port: 6379,
+//    redis_host: '192.168.155.103',
+//    redis_port: 6379,
+    redis_host: '192.168.192.38',
+    redis_port: 5360,
+    opts: {
+        hostname: 'aps.jimdb.jd.local',
+        host: 'aps.jimdb.jd.local',
+        path: '/get',
+        method: 'get',
+        port: 80
+    },
+    use_cluster: true,
     redis_db: 0,
-    session_secret: 'jd_come_on_baby@618',
-    auth_cookie_name: 'jd_come_on'
+//    redis_auth: '/redis/cluster/1206:1434093110601',
+    redis_auth: "/redis/cluster/20:1417632912164"
+//    redis_auth: "1qaz@WSX"
 };
 
 module.exports = config;
---
+++ config/test.js
@@ -0,0 +1,2 @@
+var config = {"redis_host":"192.168.155.103","redis_port":6379,"opts":{"hostname":"aps.jimdb.jd.local","host":"aps.jimdb.jd.local","path":"/get","method":"get","port":80},"use_cluster":true,"redis_db":0,"session_secret":"jd_come_on_baby@618","auth_cookie_name":"jd_come_on","cluster":[{"port":"5360","host":"192.168.192.38"},{"port":"5360","host":"192.168.192.39"}]};
+module.exports = config;
