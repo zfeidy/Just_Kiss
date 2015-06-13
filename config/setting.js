92de7dbffd6471cddaff4877548213f5c7f47502
MirrorJing <zfeidy@qq.com>
2015-06-13 15:56:41 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- config/setting.js
+++ config/setting.js
@@ -7,7 +7,7 @@ var config = {
     number: 4, //一次出现图片的张数
     expires: 360, // cookie过期时间。默认配置【0永久，1（小时），6（小时），12（小时），24（小时），48（小时），72（小时），168（小时）,360(小时),720(小时)】
     use_mongo: false, // 是否使用mongodb做存储
-    log_path: '/log/',
+    log_path: '/log',
     // 程序运行的端口
     port: 3000,
     admin: "cc95bfd3be35ec81e0b331a058118569",
