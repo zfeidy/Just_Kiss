b4b3cd51a9215a944d12e134eb760bafc6011433
zfeidy <zfeidy@qq.com>
2015-06-14 15:12:26 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	views/index.html

--- common/logger.js
+++ common/logger.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var config = require('../config/setting');
 var tools = require('../common/tools');
+var logname = "/kiss.log";
 
 if (!fs.existsSync(config.log_path)) {
     fs.mkdirSync(config.log_path);
@@ -27,8 +28,13 @@ var consolePrint = config.debug && env !== 'test';
 
 var writeLog = function (level, args) {
     var filePrint = level !== 'debug';
-    var infos = Array.prototype.slice.call(args);
-    var logStr = infos.join(" ");
+    var logStr;
+    if (args instanceof Object) {
+        logStr = JSON.stringify(args);
+    } else {
+        var infos = Array.prototype.slice.call(args);
+        logStr = infos.join(" ");
+    }
     var levelStr = level == 'error' ? "错误" : (level == 'warn' ? "警告" : (level == 'debug' ? "调试" : "一般"));
     var line = packup(tools.formatDate(new Date())) + packup(levelStr) + packup(logStr);
 
@@ -36,7 +42,7 @@ var writeLog = function (level, args) {
         console.log(line);
     }
     if (filePrint) {
-        fs.appendFile(config.log_path + "/kiss.log", line + "\n", function (err) {
+        fs.appendFile(config.log_path + logname, line + "\n", function (err) {
             console.log(err);
         });
     }
