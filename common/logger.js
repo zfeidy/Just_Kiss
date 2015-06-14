85614db66148a9b289a1e251137ba292a1cb5d36
zfeidy <zfeidy@qq.com>
2015-06-15 00:47:56 +0800

    日志工具加强

--- common/logger.js
+++ common/logger.js
@@ -28,24 +28,33 @@ var consolePrint = config.debug && env !== 'test';
 
 var writeLog = function (level, args) {
     var filePrint = level !== 'debug';
-    var logStr;
-    if (args instanceof Object) {
-        logStr = JSON.stringify(args);
-    } else {
-        var infos = Array.prototype.slice.call(args);
-        logStr = infos.join(" ");
-    }
+
     var levelStr = level == 'error' ? "错误" : (level == 'warn' ? "警告" : (level == 'debug' ? "调试" : "一般"));
-    var line = packup(tools.formatDate(new Date())) + packup(levelStr) + packup(logStr);
+    var line = packup(tools.formatDate(new Date()));
+    line += packup(levelStr);
+    line += packup(assemble(args));
 
     if (consolePrint) {
         console.log(line);
     }
     if (filePrint) {
         fs.appendFile(config.log_path + logname, line + "\n", function (err) {
-            console.log(err);
+            if (err)
+                console.log("写日志失败：" + err.message);
         });
     }
+};
+// 按参数个数封装传递过来的日志消息
+var assemble = function (args) {
+    var i = 1, msg = "";
+    if (args.length > 0) {
+        msg = args[0];
+    }
+    while (i < args.length) {
+        msg += ":" + JSON.stringify(args[i]);
+        i++;
+    }
+    return msg;
 };
 
 var packup = function (msg) {
