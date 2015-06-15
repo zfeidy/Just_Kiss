9e74040f6b67cd2039707ed43693db79bc6868d1
MirrorJing <zfeidy@qq.com>
2015-06-15 13:50:01 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- middleware/visits.js
+++ middleware/visits.js
@@ -28,3 +28,15 @@ exports.count = function (req, res, next) {
         next();
     });
 };
+
+/**
+ * 获取客户端ip地址
+ * @param {type} req
+ * @returns {reqheaders.x-forwarded-for}
+ */
+var getClientIp = function (req) {
+    return req.headers['x-forwarded-for']  // 反向代理
+            || req.connection.remoteAddress  // 远程客户端
+            || req.socket.remoteAddress   // 通过socket
+            || req.connection.socket.remoteAddress;
+};
