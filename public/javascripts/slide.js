866f29fccb0d72bca4910b832484023f8a816004
MirrorJing <zfeidy@qq.com>
2015-06-15 12:24:21 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,6 +1,6 @@
-var musicFlag = true;
 (function ($) {
-    var isclicked = false;
+    var isclicked = false,
+            musicFlag = true;
 
     $(".square").height(function () {
         return $(this).width();
@@ -155,6 +155,14 @@ var musicFlag = true;
         $(".run-type").on('tap', function (event) {
             var idx = $(".run-type").index($(this));
             $(".swiper-slide .box").attr("idx", idx);
+            // 换战线员工
+            if (useFrontRandom) {
+                render(random(employees));
+            } else {
+                init2();
+            }
+            changeSkin();
+            // 换战线员工结束
             $(".line-w").css("display", "none").eq(idx).css("display", "block");
             swiper.activeIndex = 3;
             swiper.slideTo(3, 500, false);
@@ -282,7 +290,7 @@ var musicFlag = true;
         // 发送给好友
         WeixinJSBridge.on('menu:share:appmessage', function (argv) {
             WeixinJSBridge.invoke('sendAppMessage', {
-                "img_url": "http://img12.360buyimg.com/cms/s640x960_jfs/t1021/260/561159989/47244/d897e499/5530714eN4ad9b67f.png",
+                "img_url": "http://storage.jd.com/kiss.jd.com/fenxiang.png",
                 "img_width": "640",
                 "img_height": "640",
                 "link": window.location.href,
@@ -295,7 +303,7 @@ var musicFlag = true;
         // 分享到朋友圈
         WeixinJSBridge.on('menu:share:timeline', function (argv) {
             WeixinJSBridge.invoke('shareTimeline', {
-                "img_url": "http://img12.360buyimg.com/cms/s640x960_jfs/t1021/260/561159989/47244/d897e499/5530714eN4ad9b67f.png",
+                "img_url": "http://storage.jd.com/kiss.jd.com/fenxiang.png",
                 "img_width": "640",
                 "img_height": "640",
                 "link": window.location.href,
@@ -305,7 +313,306 @@ var musicFlag = true;
                 _report('timeline', res.err_msg);
             });
         });
-    }, false)
+    }, false);
+
+    var employees = [], // 员工缓存数组
+            unkiss = [], // 未被kiss的数组
+            kissed = [], // 已被kiss的数组
+            changeAttr = [], // 皮肤数组
+            number = 4;  // 一次显示的数组长度
+    var useFrontRandom = false;// 是否使用前端随机
+
+    (function () {
+        var local = window.location.pathname;
+        $.ajax({
+            url: "http://wq.jd.com/mlogin/wxv3/LoginCheckJsonp",
+            dataType: 'JSONP',
+            jsonpCallback: 'validateLoginCallback',
+            success: function (data) {
+                if (data.iRet == "9999") {
+                    // "http://wq.jd.com/mlogin/h5v1/cpLogin_BJ?rurl="+encodeURIComponent(option.rurl)
+                    window.location.href = "http://wq.jd.com/mlogin/wxv3/login_BJ?rurl=" + encodeURIComponent(local) + "&appid=1";
+                }
+            }
+        });
+    })();
+    /**
+     * 初始化操作，如果当前没有缓存，就使用ajax拉取数据
+     * @returns {undefined}
+     */
+    var init = function () {
+        // 当前没缓存，直接使用ajax加载
+        if (employees.length === 0) {
+            $.getJSON("/cache/kiss_jd_employee", function (data) {
+                employees = data;
+                render(random(employees));
+            });
+        } else {
+            render(random(employees));
+        }
+    };
+
+    /**
+     * 随机生成指定【4】个图片
+     * @param {type} employees
+     * @returns {Array}
+     */
+    var random = function (employees) {
+        // 申明临时对象，用于存储生成的对象数据
+        var tmp = [];
+        // 如果unkiss和kissed都为空，把employees赋值到unkiss中
+        if (unkiss.length === 0 && kissed.length === 0)
+            unkiss = employees.slice(0);
+        var unkiss_length = unkiss.length;
+        // 如果unkiss的数组长度足设定值，就先从kissed数组中补充
+        var kiss_length = unkiss_length < number ? number - unkiss_length : 0;
+        var _kissed = kissed.slice(0);
+        for (var i = 0; i < kiss_length; i++) {
+            // 随机一个数据出来
+            var employee = _kissed.splice(randomIndex(_kissed.length), 1)[0];
+            // 设置kissed状态
+            employee.kissed = true;
+            tmp.push(employee);
+        }
+        // 从unkiss中填充数据到tmp
+        unkiss_length = unkiss_length < number ? unkiss_length : number;
+        var _unkiss = unkiss.slice(0);
+        for (var i = 0; i < unkiss_length; i++) {
+            // 随机一个数据出来
+            var employee = _unkiss.splice(randomIndex(_unkiss.length), 1)[0];
+            tmp.push(employee);
+        }
+        return tmp;
+    };
+
+    /**
+     * 渲染界面
+     * @param {type} checked
+     * @returns {undefined}
+     */
+    var render = function (checked) {
+        $.each(checked, function (i) {
+            var em = checked[i];
+            $("p#kiss_employee_0" + (i + 1) + " > img").attr("src", em.images.sad);
+            $("p#kiss_employee_0" + (i + 1) + " > img").attr("employeeid", em.id);
+            $("p#kiss_employee_0" + (i + 1) + " > img").attr("happyurl", em.images.happy);
+            $("p#kiss_employee_0" + (i + 1) + " > img").attr("kissed", em.kissed);
+            $("p#kiss_employee_0" + (i + 1) + " > img").attr("slogan", em.slogan);
+            $("i.kiss-num0" + (i + 1)).html(em.times);
+            if (em.kissed == true) {
+                $('.sad ul li p').eq(i).addClass("hadClick");
+                $('.sad ul li p').eq(i).children('.sad-kiss').show();
+            } else {
+                $('.sad ul li p').eq(i).removeClass("hadClick");
+                $('.sad ul li p').eq(i).children('.sad-kiss').hide();
+            }
+        });
+    };
+
+    /**
+     * 获取一个小于size的随机整数
+     * @param {type} size
+     * @returns {Number}
+     */
+    var randomIndex = function (size) {
+        return Math.floor(Math.random() * size);
+    };
+
+    /**
+     * 切换皮肤功能
+     * @param {type} event
+     * @returns {undefined}
+     */
+    var changeSkin = function () {
+        changeAttr = [];
+        for (var i = 0; i < 4; i++) {
+            var changeNum = parseInt(Math.random() * 10);
+            if (!findNum(changeNum, changeAttr)) {
+                changeAttr.push(changeNum);
+            } else {
+                i--;
+            }
+        }
+        $('.sad-cover img').each(function (index) {
+            $('.sad-cover img').eq(index).attr("src", "skin/i/sad0" + changeAttr[index] + "-cover.png");
+        });
+    };
+
+    var findNum = function (num, arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    /**
+     * 点击人物切换动画的操作
+     * @param {type} target 点击人物所对应的P节点
+     * @returns {undefined}
+     */
+    var clickOne = function (target) {
+        if (target.hasClass('hadClick')) {
+            return;
+        }
+        var happyurl = target.children('img').attr("happyurl");
+        var slogan = target.children('img').attr("slogan");
+        $('.sad-popup .sp-people img').attr("src", happyurl);
+        $('.sad-popup .sp-txt').html(slogan);
+        target.addClass('hadClick');
+        target.children('.sad-kiss').show();
+        /*$('.sad-swing-hide').removeClass();*/
+        target.find('.sad-swing-hide').removeClass();
+        musicFlag && $("#kissaudio")[0].play();
+        setTimeout(function () {
+            $('.sad-popup').show();
+            /*弹窗动画*/
+            setTimeout(function () {
+                $('.sp-people').show();
+                $('.sp-people').addClass('happy-tada');
+                setTimeout(function () {
+                    $('.sp-skin01,.sp-del,.sp-txt').show();
+                    $('.sp-skin01,.sp-del,.sp-txt').addClass('happy-fadeIn');
+                    setTimeout(function () {
+                        $('.happy-point').show();
+                        $('.happy-point').addClass('happy-bounceInLeft');
+                    }, 500);
+                }, 1000);
+            }, 1000);
+        }, 800);
+    };
+
+    /**
+     * kiss操作
+     * @param {employee} target
+     * @param {boolean} type true标示前端随机结果的kiss操作，false标示后端随机
+     * @returns {undefined}
+     */
+    var kissOne = function (target, type) {
+        var thisImg = target.children("img");
+        var kissed = thisImg.attr("kissed");
+        var emid = thisImg.attr("employeeid");
+        if (kissed != "true") {
+            $.ajax({
+                url: '/employee/kiss',
+                type: 'post',
+                data: {
+                    id: emid,
+                    withline: true
+                },
+                success: function (data) {
+                    if (data.success) {
+                        thisImg.attr("kissed", true);
+                        var num = data.msg;
+                        target.children("i").html(num);
+                        if (type) {
+                            if (unkiss.length !== 0) {
+                                var id = emid;
+                                kissme(id, num);
+                            }
+                        }
+                    }
+                }
+            });
+            luckdraw("kissIT11");
+        }
+    };
+
+    /**
+     * 后台随机算法进行初始化
+     * @returns {undefined}
+     */
+    var init2 = function () {
+        var line = $(".swiper-slide .box").attr("idx");
+        $.ajax({
+            url: "/employee/random",
+            type: "post",
+            dataType: "json",
+            data: {
+                line: line ? (Number(line) + 1) : 1
+            },
+            success: function (data) {
+                if (data.success) {
+                    render(data.msg);
+                }
+            }
+        });
+    };
+
+    // 初始化
+    (function () {
+        if (useFrontRandom) {
+            init();
+        } else {
+            init2();
+        }
+    })();
+
+    /**
+     * 点击图片后的操作
+     * @param {type} data
+     * @returns {undefined}
+     */
+    $(".change").click(function () {
+        if (useFrontRandom) {
+            render(random(employees));
+        } else {
+            init2();
+        }
+        changeSkin();
+    });
+
+    /**
+     * 点击人物层的操作：
+     * 第一个是切换动画（打开弹出层，切换人物图片）
+     * 第二个是执行一个ajax操作
+     */
+    $('.sad ul li p').click(function () {
+        clickOne($(this));
+        kissOne($(this), useFrontRandom);
+    });
+
+    var luckdraw = function (active) {
+        $.ajax({
+            url: 'http://wq.jd.com/active/active_draw?active=' + active,
+            dataType: 'JSONP',
+            jsonpCallback: 'ActiveLotteryCallBack',
+            success: function (data) {
+                if (data) {
+                    if (data.ret == 2) {
+                        var local = window.location.pathname;
+                        // "http://wq.jd.com/mlogin/h5v1/cpLogin_BJ?rurl="+encodeURIComponent(local)
+                        window.location.href = "http://wq.jd.com/mlogin/wxv3/login_BJ?rurl=" + encodeURIComponent(local) + "&appid=1";
+                    } else
+                    if (data.ret == 0 && data.bingo.ret == 0) {
+                        $(".happy-point .hp-prize").html("<p>初次见面，送你</p><p class='hp-big'>" + data.award.awardcode + "</p>").show().siblings("div").hide();
+                    } else if (data.ret == 3) {
+                        $(".happy-point .hp-noprize").html("<p>木有中奖哟</p><p>换个人kiss吧</p>").show().siblings("div").hide();
+                    } else if (data.ret == 104) {
+                        $(".happy-point .hp-noprize").html("<p>您来晚啦</p><p>活动已结束</p>").show().siblings("div").hide();
+                    } else if (data.ret == 157) {
+                        $(".happy-point .hp-noprize").html("<p>抽奖机会已用完</p><p>分享好友一起kiss</p>").show().siblings("div").hide();
+                    } else {
+                        $(".happy-point .hp-nochange").show().siblings("div").hide();
+                    }
+                }
+            }
+        });
+    };
+
+    // kiss操作，把已经做了kiss操作的图片放置到kisssed数组
+    var kissme = function (id, num) {
+        console.log("id = " + id);
+        for (var i = 0; i < unkiss.length; i++) {
+            if (unkiss[i].id == id) {
+                // 把数据从unkiss中移动到kissed中
+                var one = unkiss.splice(i, 1)[0];
+                one.times = num;
+                kissed.push(one);
+            }
+        }
+    };
 })(Zepto);
 
 
