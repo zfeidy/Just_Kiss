ce3b5401a8cb103af44003ae6b473a7d1609d0b0
MirrorJing <zfeidy@qq.com>
2015-06-18 19:29:38 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/redis.js
    	controller/employee.js
    	router.js
    	views/employees/add.html
    	views/employees/list.html
    	views/kissme/random.html

--- middleware/visits.js
+++ middleware/visits.js
@@ -6,24 +6,25 @@ exports.count = function (req, res, next) {
     var sessionid = req.sessionID;
     var kissed_session = "kissed_" + sessionid;
     redis.get(kissed_session, function (err, data) {
-        if(err){
+        if (err) {
             logger.error("异常: ", err);
             return next(err);
         }
         if (!data) {
             redis.set(kissed_session, 0, function (err, data) {
-                if (err){
+                if (err) {
                     logger.error("异常: ", err);
                     return next(err);
                 }
             });
 
             redis.incrby(kissed_counter, 1, function (err, data) {
-                if (err){
+                if (err) {
                     logger.error("异常: ", err);
                     return next(err);
                 }
             });
+            console.info("当前的用户IP为", getClientIp(req));
         }
         next();
     });
